3dbc687e4591f7cf7975b94d30a0e7ace1738c91
Amit Kumar Thakur <amit103065@gmail.com>
2023-12-10 10:18:08 +0400

    Added sql command to check whole db size

--- check-size-of-all-tables.sql
+++ check-size-of-all-tables.sql
@@ -1,6 +1,17 @@
-SELECT 
-    table_name AS `Table`, 
-    round(((data_length + index_length) / 1024 / 1024), 2) `Size in MB` 
+
+-- Check size of all databases in MB:
+
+SELECT table_schema AS "Database", 
+ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) AS "Size (MB)" 
 FROM information_schema.TABLES 
-WHERE table_schema = "magento2"
+GROUP BY table_schema;
+
+
+-- Check Size of all Tables in MB for magento2 database:
+
+SELECT table_name AS "Table",
+ROUND(((data_length + index_length) / 1024 / 1024), 2) AS "Size (MB)"
+FROM information_schema.TABLES
+WHERE table_schema = "database_name"
 ORDER BY (data_length + index_length) DESC;
+
